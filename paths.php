f740edef201e2b84cf29eecaa6a93b9cd2692809
Muhammad Usman Sarwar <muhammadusmanserwer@yahoo.com>
2021-03-12 23:09:55 +0500

    class template

---
+++ paths.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ *
+ * This class helps developers make standardized calls across their entire
+ * application. This class was found by (Muhammad Usman Sarwar)
+ *
+ *
+ * @link              https://github.com/muhammadusmansarwar/path_scanner
+ * @version           1.0.0
+ * Description:       This cla
+ * Last Update:       2021-03-12
+ * @author:            Muhammad Usman Sarwar <muhammadusmanserwer@gmail.com>
+ * 
+ * @access public
+ * @return array
+
+ * @license:           MIT
+ * License URI:       https://opensource.org/licenses/MIT
+ */
+
+/**
+ * 
+ * 
+ * 
+ * @package    PATHS
+ * @subpackage Controller
+ */
+class paths
+{
+
+    /**
+     * 
+     * Uploads a file
+     *
+     * @param string $root  description
+     * @param string $dummy  description
+     * @param integer $dummy  description
+     * @param integer $dummy  description
+     * @param string $dummy  description
+     * @return boolean
+     */
+
+    function __construct()
+    {
+    }
+    public function __invoke($var = null): bool
+    {
+        return false;
+    }
+    function __toString()
+    {
+        # code...
+    }
+}
+
+
+/**
+ *
+
+ * @param string (Name of the procedure)
+ * @param bool $object (true returns object)
+
+ */
